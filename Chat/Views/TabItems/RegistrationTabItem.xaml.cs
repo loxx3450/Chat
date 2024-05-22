7c081f10196b577f8a5a5f0ba28482db3e5682dc
loxx3450 <egorka231231@gmail.com>
2024-05-23 00:00:16 +0300

    temp.2.1. addition of test RegistrationTabItem

---
+++ Chat/Views/TabItems/RegistrationTabItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chat.Views.TabItems
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationTabItem.xaml
+    /// </summary>
+    public partial class RegistrationTabItem : TabItem
+    {
+        public RegistrationTabItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
