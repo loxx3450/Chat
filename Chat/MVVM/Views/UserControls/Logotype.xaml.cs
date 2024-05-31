c45d2c327717b294bc623ee964aa44ddbcce8f71
loxx3450 <egorka231231@gmail.com>
2024-05-31 13:36:07 +0300

    dev.1.24. addition of new UserControl: Logotype

---
+++ Chat/MVVM/Views/UserControls/Logotype.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chat.MVVM.Views.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Logotype.xaml
+    /// </summary>
+    public partial class Logotype : UserControl
+    {
+        public new DependencyProperty HeightProperty =
+            DependencyProperty.Register(nameof(Height), typeof(double), typeof(Logotype),
+                new PropertyMetadata(0.0, (d, e) => (d as Logotype).Logo.Height = (double)e.NewValue));
+
+        public new double Height
+        {
+            get => (double)GetValue(HeightProperty);
+            set => SetValue(HeightProperty, value);
+        }
+
+
+        public Logotype()
+        {
+            InitializeComponent();
+        }
+    }
+}
