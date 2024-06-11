a6ffa634042c64f06c4f8801b5769735508dfd0b
loxx3450 <egorka231231@gmail.com>
2024-06-11 22:49:12 +0300

    dev.1.37. addition of new View and its ViewModel => RecoveryView

---
+++ Chat/MVVM/ViewModels/RecoveryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Chat.MVVM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.MVVM.ViewModels
+{
+    public class RecoveryViewModel : ViewModelBase
+    {
+
+    }
+}
