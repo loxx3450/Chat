3a2446e218a05cdfef9a4a3919dac28ddabd685e
loxx3450 <egorka231231@gmail.com>
2024-06-23 22:24:11 +0300

    dev.1.45. validation of email works

--- Chat/MVVM/ViewModels/LoginViewModel.cs
+++ Chat/MVVM/ViewModels/LoginViewModel.cs
@@ -1,14 +1,38 @@
 ﻿using Chat.MVVM.Core;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Chat.MVVM.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        private string _email;
+
+        [Required(ErrorMessage = "Can not be empty")]
+        [RegularExpression(@"^\w+@[a-zA-Z]+\.[a-zA-Z]+", ErrorMessage = "Email is invalid")]
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                ValidateProperty(value, nameof(Email));
+                SetField(ref _email, value);
+            }
+        }
+
+        private string _password; 
+        public string Password
+        {
+            get => _password;
+            set => SetField(ref _password, value);
+        }
+
         public RelayCommand NavigateToRegistrationCommand { get; set; }
         public RelayCommand NavigateToRecoveryCommand { get; set; }
 
