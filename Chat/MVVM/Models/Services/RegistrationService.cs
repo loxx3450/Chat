1d1ce3772a594a611e49c7a9704361f040ed5859
loxx3450 <egorka231231@gmail.com>
2024-06-30 12:03:33 +0300

    temp.3.3. Registration request works

---
+++ Chat/MVVM/Models/Services/RegistrationService.cs
@@ -0,0 +1,24 @@
+﻿using CommonLibrary;
+using CommonLibrary.Models;
+using CommonLibrary.Payloads.Registration;
+using ProtocolLibrary.Message;
+using SocketEventLibrary.SocketEventMessageCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.MVVM.Models.Services
+{
+    public class RegistrationService
+    {
+        public static void Register(User user) 
+        {
+            ProtocolMessage message = new ProtocolMessage();
+            message.SetPayload(new RegistrationRequestPayload(user));
+
+            SocketEventHandler.Emit(new SocketEventProtocolMessage(MessageType.RegistrationRequest, message));
+        }
+    }
+}
