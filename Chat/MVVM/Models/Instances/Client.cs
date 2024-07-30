969266caa15d84c6276825ebeddc7e3df7b11ac9
loxx3450 <egorka231231@gmail.com>
2024-07-30 11:35:54 +0300

    temp.4.17. addition of Client

---
+++ Chat/MVVM/Models/Instances/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.MVVM.Models.Instances
+{
+    internal static class Client
+    {
+        public static int AssociatedUserId { get; set; }
+    }
+}
