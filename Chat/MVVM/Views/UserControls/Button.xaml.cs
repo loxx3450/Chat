88919458b8e500bbc3cb5465d5ea38d670f73c78
loxx3450 <egorka231231@gmail.com>
2024-07-31 14:13:30 +0300

    dev.1.76. addition of new UserControl: Button

---
+++ Chat/MVVM/Views/UserControls/Button.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Controls;
+
+namespace Chat.MVVM.Views.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Button.xaml
+    /// </summary>
+    public partial class Button : UserControl
+    {
+        //Command
+        public static DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(Button));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+
+        //Background
+        public static new DependencyProperty BackgroundProperty =
+            DependencyProperty.Register(nameof(Background), typeof(Brush), typeof(Button));
+
+        public new Brush Background
+        {
+            get => (Brush)GetValue(BackgroundProperty);
+            set => SetValue(BackgroundProperty, value);
+        }
+
+
+        //Appearance
+        public static DependencyProperty AppearanceProperty =
+            DependencyProperty.Register(nameof(Appearance), typeof(ControlAppearance), typeof(Button));
+
+        public ControlAppearance Appearance
+        {
+            get => (ControlAppearance)GetValue(AppearanceProperty);
+            set => SetValue(AppearanceProperty, value);
+        }
+
+
+        //Text
+        public static DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(Button));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+
+        public Button()
+        {
+            InitializeComponent();
+        }
+    }
+}
