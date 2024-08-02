43397694404c69ebd71a6fc302fe1e2d3990d8aa
loxx3450 <egorka231231@gmail.com>
2024-08-02 17:15:35 +0200

    dev.1.88. addition of new UserControl: LoadingSpinner

---
+++ Chat/MVVM/Views/UserControls/AdditionalInfrastructure/Converters/DiameterAndThicknessToStrokeDashArrayConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Chat.MVVM.Views.UserControls.AdditionalInfrastructure.Converters
+{
+    public class DiameterAndThicknessToStrokeDashArrayConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return 0;
+
+            double diameter = (double)values[0];
+            double thickness = (double)values[1];
+
+            double circumference = Math.PI * diameter;
+
+            double lineLength = circumference * 0.77;
+            double gapLength = circumference * 0.23;
+
+            return new DoubleCollection(new[] { lineLength / thickness, gapLength / thickness });
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
