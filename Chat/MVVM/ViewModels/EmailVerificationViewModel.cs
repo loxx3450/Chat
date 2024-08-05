1bf65dbe54358fee3e83d9d324d7097d7df868fd
loxx3450 <egorka231231@gmail.com>
2024-08-05 18:11:25 +0200

    temp.5.13. realization of EmailVerificationView and -ViewModel

--- Chat/MVVM/ViewModels/EmailVerificationViewModel.cs
+++ Chat/MVVM/ViewModels/EmailVerificationViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Chat.MVVM.Core;
+using Chat.MVVM.Models.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,42 @@ namespace Chat.MVVM.ViewModels
 {
     internal class EmailVerificationViewModel : ViewModelBase
     {
+        private string _code = string.Empty;
+
+        [Required(ErrorMessage = "Can not be empty")]
+        [RegularExpression(@"^[A-Z0-9]{6}$", ErrorMessage = "Code is invalid")]
+        public string Code
+        {
+            get => _code;
+            set => SetValidatedField(ref _code, value, nameof(Code));
+        }
+
+
+        public RelayCommand VerifyCommand { get; set; }
+        public RelayCommand NavigateToRegistrationCommand { get; set; }
+
+
+        public EmailVerificationViewModel()
+        {
+            NavigateToRegistrationCommand = new RelayCommand((o) => Navigator.NavigateTo<RegistrationViewModel>());
+
+            VerifyCommand = new RelayCommand(Verify, CanVerify);
+        }
+
+        private void Verify(object obj)
+        {
+
+        }
+
+        private bool CanVerify(object obj)
+        {
+            return !HasErrors
+                && !string.IsNullOrEmpty(_code);
+        }
+
         public override void ResetData()
         {
-            throw new NotImplementedException();
+            _code = string.Empty;
         }
     }
 }
