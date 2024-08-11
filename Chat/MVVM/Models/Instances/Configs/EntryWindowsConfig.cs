599aa2bcbd8fdf54a8a44368b4a9735d3d075687
loxx3450 <egorka231231@gmail.com>
2024-08-11 13:06:52 +0200

    temp.7.5. values for ColumnStyles are removed from EntryWindows.config

--- Chat/MVVM/Models/Instances/Configs/EntryWindowsConfig.cs
+++ Chat/MVVM/Models/Instances/Configs/EntryWindowsConfig.cs
@@ -20,17 +20,5 @@ namespace Chat.MVVM.Models.Instances.Configs
         //Views parameters
         public static double ContentWidth => Convert.ToDouble(config.AppSettings.Settings[nameof(ContentWidth)].Value);
         public static double LogotypeHeight => Convert.ToDouble(config.AppSettings.Settings[nameof(LogotypeHeight)].Value);
-
-
-        //Side column's style parameters
-        public static double SideColumnHorizontalScreenValue => Convert.ToDouble(config.AppSettings.Settings[nameof(SideColumnHorizontalScreenValue)].Value);
-        public static double SideColumnSquareScreenValue => Convert.ToDouble(config.AppSettings.Settings[nameof(SideColumnSquareScreenValue)].Value);
-        public static double SideColumnVerticalScreenValue => Convert.ToDouble(config.AppSettings.Settings[nameof(SideColumnVerticalScreenValue)].Value);
-
-
-        //Main column's style parameters
-        public static double MainColumnHorizontalScreenValue => Convert.ToDouble(config.AppSettings.Settings[nameof(MainColumnHorizontalScreenValue)].Value);
-        public static double MainColumnSquareScreenValue => Convert.ToDouble(config.AppSettings.Settings[nameof(MainColumnSquareScreenValue)].Value);
-        public static double MainColumnVerticalScreenValue => Convert.ToDouble(config.AppSettings.Settings[nameof(MainColumnVerticalScreenValue)].Value);
     }
 }
