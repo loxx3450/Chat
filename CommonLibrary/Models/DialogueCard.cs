a59c63c518c754317cc6515ccfb4cac0247923e1
loxx3450 <egorka231231@gmail.com>
2024-08-19 11:34:43 +0200

    temp.8.4. addition of DialogueCard

---
+++ CommonLibrary/Models/DialogueCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Models
+{
+    public class DialogueCard
+    {
+        public string DialogueName { get; set; }
+
+        public byte[]? IconStream { get; set; }
+
+        public Message? LastMessage { get; set; }
+
+        public DialogueCard(string dialogueName, byte[]? iconStream = null, Message? lastMessage = null)
+        {
+            DialogueName = dialogueName;
+
+            IconStream = iconStream;
+
+            LastMessage = lastMessage;
+        }
+    }
+}
