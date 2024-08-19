59afeaae75dcd721b2dd1a9b4fa9be86712d2004
loxx3450 <egorka231231@gmail.com>
2024-08-19 11:41:41 +0200

    temp.8.5. addition of new Converter: BitStream to BitmapImage

---
+++ Chat/MVVM/Views/AdditionalInfrastructure/Converters/BitStreamToBitmapImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Chat.MVVM.Views.AdditionalInfrastructure.Converters
+{
+    public class BitStreamToBitmapImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+
+            BitmapImage bitmap = new BitmapImage();
+
+            MemoryStream stream = new MemoryStream((byte[])value);
+
+            bitmap.BeginInit();
+            bitmap.StreamSource = stream;
+            bitmap.EndInit();
+
+            return bitmap;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
