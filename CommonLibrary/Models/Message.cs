e2d23d276508d26d0b5a8a63525d3b412799fba1
loxx3450 <egorka231231@gmail.com>
2024-08-19 11:24:09 +0200

    temp.8.3. addition of Message

---
+++ CommonLibrary/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Models
+{
+    public class Message
+    {
+        public string? Text { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool HasFiles { get; set; }
+
+        public Message(string? text, DateTime sentAt, bool hasFiles = false)
+        {
+            Text = text;
+            SentAt = sentAt;
+            HasFiles = hasFiles;
+        }
+    }
+}
