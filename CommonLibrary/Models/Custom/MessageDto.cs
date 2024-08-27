570179d8618be84f6bb717c5262e2be5f63b1012
loxx3450 <egorka231231@gmail.com>
2024-08-27 17:30:50 +0200

    temp.9.4. addition of FileDto and MessageDto

---
+++ CommonLibrary/Models/Custom/MessageDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Models.Custom
+{
+    public class MessageDto
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime SentAt { get; set; }
+
+        public string? Text { get; set; }
+
+        public List<FileDto>? Files { get; set; }
+
+
+        public MessageDto(int id, int userId, DateTime sentAt, string? text, List<FileDto>? files = null) 
+        { 
+            Id = id;
+            UserId = userId;
+            SentAt = sentAt;
+            Text = text;
+            Files = files;
+        }
+    }
+}
