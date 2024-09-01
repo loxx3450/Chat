6564101c462e909f117f9c5b92589d39feacaf6d
loxx3450 <egorka231231@gmail.com>
2024-09-01 13:52:17 +0200

    temp.9.7. addition of IsMyMessageConverter

---
+++ Chat/Styles/AdditionalInfrastructure/Converters/IsMyMessageConverter.cs
@@ -0,0 +1,27 @@
+﻿using Chat.MVVM.Models.Instances;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Chat.Styles.AdditionalInfrastructure.Converters
+{
+    public class IsMyMessageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int messageOwnerId = (int)value;
+
+            return messageOwnerId == Client.AssociatedUserId;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
